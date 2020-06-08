c29d8106b0a47da7255374d03cee5c2911b48160
Luis Manuel Ramirez Vargas <luis.manuel.ramirez.91@gmail.com>
2020-06-08 16:19:56 -0500

    404 page

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,10 @@
  */
 
 // You can delete this file if you're not using it
+import "jquery/dist/jquery.min.js"
+import "popper.js/dist/popper.min"
+import "bootstrap/dist/js/bootstrap.min.js"
+
 require("prismjs/themes/prism.css")
 //require("./src/sass/prism-vsc-dark-plus.css")
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,8 +7,12 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="container" style={{ height: '500px'}}>
+      <h1><i className="fas fa-exclamation-triangle"></i> 404</h1>
+      <h2>Something is Wrong</h2>
+      <p>The page you are looking for was moved, removed, renamed or might never existed.</p>
+      <Link to="/" className="btn btn-primary">Go Home <i className="fas fa-chevron-right"></i></Link>
+    </div>
   </Layout>
 )
 
